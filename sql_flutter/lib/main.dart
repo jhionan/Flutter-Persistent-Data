1f75c72f28ff018327eac6b4f34db108434dfc36
Jhionan <Jhionan@gmail.com>
2018-06-20 23:02:24 -0300

    DB CRUD

--- sql_flutter/lib/main.dart
+++ sql_flutter/lib/main.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:sqflite/sqflite.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:sql_flutter/models/user.dart';
+import 'package:sql_flutter/utils/database_helper.dart';
+
+List _users;
+main()async {
+  var db = new DatabaseHelper();
+_users = await db.getAllUsers();
+ int conter = await db.getCount();
+  int savedUser = await db.addUser(new User("Jhionan $conter","Rian"));
+  db.updateUser(new User("updated", "updated"),2);
+  
+  var updatedUser = await db.updateUserFromMap(new User.fromMap({"username":"updated2",
+  'password':'updatedpassword','id':3}));
+  print("$updatedUser");
+  print("$savedUser users saved");
+
+  //db.clearDb();
+  for(var i in _users){
+    User user = User.map(i);
+    print("User ${user.username} ${user.password}, ${user.id}");
+  }
 
 
-main()=>runApp(MaterialApp(
+  runApp(MaterialApp(
   title: "SQFLITE",
   home: new Home(),
 ));
+
+}
 
 class Home extends StatelessWidget {
   @override
@@ -14,7 +35,23 @@ class Home extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text("SQFLITE test"),
       backgroundColor: Colors.black54,),
-
+body: new ListView.builder(
+  itemCount: _users.length,
+  itemBuilder:(BuildContext context,int position){ //or itemBuilder:(_,int position)
+    return new Card(
+      color: Colors.white70,
+      elevation: 2.0,
+      child: new ListTile(
+        title: new Center(
+          child: Text("User: ${User.fromMap(_users[position]).username}"),
+        ),
+        subtitle: Center(
+          child: Text("Password ${User.map(_users[position]).password} id: ${User.map(_users[position]).id}"),
+        ),
+      ),
+    );
+  },
+  )
     );
   }
 }
--- sql_flutter/lib/utils/database_helper.dart
+++ sql_flutter/lib/utils/database_helper.dart
@@ -4,10 +4,11 @@ import 'dart:io';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:sql_flutter/models/user.dart';
 
 class DatabaseHelper{
   static final DatabaseHelper _instance  = new DatabaseHelper.internal();
-  final String userTable;
+  final String userTable = "users";
   final String columnId = "id";
   final String columnUserName = "username";
   final String columnPassword = "password";
@@ -22,6 +23,7 @@ class DatabaseHelper{
       return _db;
     }
     _db = await initDb();
+    return _db;
   }
 
 
@@ -31,6 +33,7 @@ class DatabaseHelper{
     Directory documentosDirectory = await getApplicationDocumentsDirectory();
     String path = join(documentosDirectory.path,"maindb.db");
     var myDb = await openDatabase(path,version: 1,onCreate: _onCreate);
+    return myDb;
   }
 
 
@@ -41,4 +44,59 @@ class DatabaseHelper{
           "$columnPassword TEXT) "
     );
   }
+
+
+  //Insertion
+Future<int> addUser(User user)async {
+  var dbCliente = await db;
+
+  int res = await dbCliente.insert(userTable, user.toMap());
+  return res;
+  }
+
+Future<List> getAllUsers() async{
+    var dbClient = await db;
+    var result = await dbClient.rawQuery("SELECT * FROM $userTable");
+    return result;
+}
+
+Future<int> getCount() async{
+    var dbClient = await db;
+    return Sqflite.firstIntValue(await dbClient.rawQuery("SELECT COUNT(*) FROM $userTable"));
+}
+
+Future<User> getUser(int id)async{
+    var dbClient = await db;
+    var userList = await dbClient.rawQuery("SELECT * FROM $userTable WHERE $columnId = $id");
+
+    if (userList.length==0) return null;
+    return new User.fromMap(userList.first);
+}
+
+Future<int> deleteUser(int id)async{
+    var dbClient = await db;
+    return await dbClient.delete(userTable,where: "$columnId = ?",whereArgs: [id]);
+}
+
+Future<int> updateUser(User user, int id)async{
+    var dbClient = await db;
+    return await dbClient.update(userTable, user.toMap(),where: "$columnId =?",whereArgs: [id]);
+}
+
+  Future<int> updateUserFromMap(User user)async{
+    var dbClient = await db;
+    return await dbClient.update(userTable, user.toMap(),where: "$columnId =?",whereArgs: [user.id]);
+  }
+
+Future close()async{
+    var dbClient = await db;
+    return dbClient.close();
+}
+
+  Future clearDb()async{
+    var dbClient = await db;
+    return dbClient.rawDelete("DELETE FROM $userTable");
+}
+
+
 }
